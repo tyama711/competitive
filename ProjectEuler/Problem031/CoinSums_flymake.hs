38c970912fcccaf3a1f3a3edc87c63caa9145370
tyama711 <tyama711@gmail.com>
2017-01-16 23:17:49 +0900

    remove unnecessary files

--- ProjectEuler/Problem031/CoinSums_flymake.hs
+++
@@ -1,9 +0,0 @@
-import Control.Applicative
-import Control.Monad
-import Data.List
-
-solve :: Int -> [Int] -> IO Int
-solve n (c:cs) = if c == 1
-then return 1
-else sum <$> (forM [0..(n `div` c)] $ \x -> solve (n-x*c) cs)
-main = print =<< solve 200 [200, 100, 50, 20, 10, 5, 2, 1]
